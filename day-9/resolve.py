0339009d488caafd8a12f6df1ba9243d0ad01345
David Corticchiato <david.corticchiato@gmail.com>
2020-12-09 16:30:54 -0500

    day 9

---
+++ day-9/resolve.py
@@ -0,0 +1,32 @@
+import sys
+
+
+sum_combinations = lambda numbers: [number + other for index, number in enumerate(numbers) for other in numbers[:index]]
+
+
+def find_weakness(numbers, preambule):
+    for index, number in enumerate(numbers):
+        if index >= preambule and not number in sum_combinations(numbers[index - preambule:index]):
+            return number
+
+
+def find_encryption_weakness(numbers, weakness):
+    for left_index, number in enumerate(numbers):
+        contiguous_sum = 0
+        right_index = left_index + 1
+
+        while contiguous_sum < weakness:
+            contiguous_numbers = numbers[left_index:right_index + 1]
+            contiguous_sum = sum(contiguous_numbers)
+
+            if len(contiguous_numbers) > 1 and contiguous_sum == weakness:
+                return min(contiguous_numbers) + max(contiguous_numbers)
+
+            right_index += 1
+
+
+input_numbers = [int(line.strip()) for line in sys.stdin.readlines()]
+weakness = find_weakness(input_numbers, int(sys.argv[1]))
+
+print("solution 1:", weakness)
+print("solution 2:", find_encryption_weakness(input_numbers, weakness))
