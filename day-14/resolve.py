5df33bb0232c5a63c14f73a7fc868634c527e03f
David Corticchiato <david.corticchiato@gmail.com>
2020-12-14 10:34:18 -0500

    lint day 14

--- day-14/resolve.py
+++ day-14/resolve.py
@@ -16,19 +16,25 @@ def sum_mem_values(lines, version=1):
         if mask_match:
             mask_expression = mask_match[1]
             or_mask = int(mask_expression.replace('X', '0'), 2)
-            and_mask = int(mask_expression.replace('0', '1').replace('X', '0') if version == 2 else mask_expression.replace('X', '1'), 2)
-            floating_increment_parts = [2 ** (len(line) - index - 2) for index, char in enumerate(line) if char == 'X']
-            floating_increments = [0] + sorted([sum(combination) for length in range(1, len(floating_increment_parts) + 1) for combination in combinations(floating_increment_parts, length)])
+            and_mask = int(mask_expression.replace('0', '1').replace('X', '0') 
+                if version == 2 else mask_expression.replace('X', '1'), 2)
+            floating_increment_parts = [2 ** (len(line) - index - 2)
+                                        for index, char in enumerate(line) if char == 'X']
+            floating_increments = [0] + sorted([sum(combination)
+                                    for length in range(1, len(floating_increment_parts) + 1)
+                                    for combination in combinations(floating_increment_parts, length)])
         else:
             instruction = MEMSET_PATTERN.match(line).groupdict()
+            value = int(instruction['value'])
+            address = int(instruction['address'])
 
             if version == 1:
-                mem[instruction['address']] = (int(instruction['value']) | or_mask) & and_mask
+                mem[address] = (value | or_mask) & and_mask
             elif version == 2:
-                base_address = (int(instruction['address']) | or_mask) & and_mask
+                base_address = (address | or_mask) & and_mask
 
                 for floating_increment in floating_increments:
-                    mem[base_address + floating_increment] = int(instruction['value'])
+                    mem[base_address + floating_increment] = value
     
     return sum(mem.values())
 
