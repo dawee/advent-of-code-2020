77d39166dac5399bca6cfd6cdeac9c6b891bccf7
David Corticchiato <david.corticchiato@gmail.com>
2020-12-14 11:01:24 -0500

    better human pep8

--- day-14/resolve.py
+++ day-14/resolve.py
@@ -16,12 +16,13 @@ def sum_mem_values(lines, version=1):
         if mask_match:
             mask_expression = mask_match[1]
             or_mask = int(mask_expression.replace('X', '0'), 2)
-            and_mask = int(mask_expression.replace('0', '1').replace(
-                'X', '0') if version == 2 else mask_expression.replace('X', '1'), 2)
-            floating_increment_parts = [
-                2 ** (len(line) - index - 2) for index, char in enumerate(line) if char == 'X']
-            floating_increments = [0] + sorted([sum(combination) for length in range(1, len(
-                floating_increment_parts) + 1) for combination in combinations(floating_increment_parts, length)])
+            and_mask = int(mask_expression.replace('0', '1').replace('X', '0')
+                           if version == 2 else mask_expression.replace('X', '1'), 2)
+            floating_increment_parts = [2 ** (len(line) - index - 2)
+                                        for index, char in enumerate(line) if char == 'X']
+            floating_increments = [0] + sorted([sum(combination)
+                                                for length in range(1, len(floating_increment_parts) + 1)
+                                                for combination in combinations(floating_increment_parts, length)])
         else:
             instruction = MEMSET_PATTERN.match(line).groupdict()
             value = int(instruction['value'])
